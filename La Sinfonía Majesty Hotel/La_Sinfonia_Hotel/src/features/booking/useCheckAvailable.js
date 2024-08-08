236cab624b1d9b6ab47a9a2667b77c4d68890a4f
Nguyễn Văn Hiếu <124571611+Hieudeptraivc@users.noreply.github.com>
2024-08-08 18:44:32 +0700

    Update useCheckAvailable.js

--- La Sinfonía Majesty Hotel/La_Sinfonia_Hotel/src/features/booking/useCheckAvailable.js
+++ La Sinfonía Majesty Hotel/La_Sinfonia_Hotel/src/features/booking/useCheckAvailable.js
@@ -2,14 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 // import { useParams } from "react-router-dom";
 import { getBookingsByRoom } from "../../services/apiBooking";
 
-export function useCheckAvailable(roomId) {
+export function useCheckAvailable(roomId,dateStart) {
   const {
     isPending,
     data: bookingCheck,
     isError,
   } = useQuery({
     queryKey: ["bookings", Number(roomId)],
-    queryFn: () => getBookingsByRoom({ roomId }),
+    queryFn: () => getBookingsByRoom({ roomId,dateStart }),
   });
 
   const bookingList = bookingCheck?.data.map((booking) => ({
